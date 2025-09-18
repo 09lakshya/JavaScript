5b77d97e8c226f57adaa2a8fd4f0b578356ad3ca
Lakshya <lakshyahindocha@gmail.com>
2025-09-18 21:32:43 +0530

    strings basic

--- strings.js
+++ strings.js
@@ -2,3 +2,5 @@ let name = "Lakshya"
 let age = "19"
 let gender = "Male"
 console.log(`My name is ${name}, I'm ${age} years old. I'm ${gender}`);
+// another way is console.log(name  + age + gender);
+// to access an index console.log(name[index])
