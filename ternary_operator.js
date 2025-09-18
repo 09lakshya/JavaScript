54e0523171d5f59d3ddbc18b81b3ebb3b64b4ad2
Lakshya <lakshyahindocha@gmail.com>
2025-09-18 23:28:36 +0530

    ternary operator

--- ternary_operator.js
+++ ternary_operator.js
@@ -1,7 +1,7 @@
-let a = prompt("Enter your age: ")
-a = Number.parseInt(a)
-let b = (a>=18)? "You can drive" : "You cannot drive"
-console.log(b)
+let a = prompt("Enter your age: ");
+a = Number.parseInt(a);
+let b = a >= 18 ? "You can drive" : "You cannot drive";
+console.log(b);
 /*
 Another method:
 console.log((a>=18)? "You can drive" : "You cannot drive")
