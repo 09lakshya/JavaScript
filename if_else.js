55366cf242ebabcb5d37233ede82fe4c150db216
Lakshya <lakshyahindocha@gmail.com>
2025-09-18 23:26:23 +0530

    if else

--- if_else.js
+++ if_else.js
@@ -1,19 +1,14 @@
-let a = prompt("Enter a number between 1 to 5: ")
-if(a==1) {
-    alert("You chose English !")
-}
-else if(a==2) {
-    alert("You chose Spanish !")
-}
-else if(a==3) {
-    alert("You chose Hindi !")
-}
-else if(a==4) {
-    alert("You chose Arabic !")
-}
-else if(a==5) {
-    alert("You chose Gujarati !")
-}
-else {
-    alert("WRONG INPUT")
+let a = prompt("Enter a number between 1 to 5: ");
+if (a == 1) {
+  alert("You chose English !");
+} else if (a == 2) {
+  alert("You chose Spanish !");
+} else if (a == 3) {
+  alert("You chose Hindi !");
+} else if (a == 4) {
+  alert("You chose Arabic !");
+} else if (a == 5) {
+  alert("You chose Gujarati !");
+} else {
+  alert("WRONG INPUT");
 }
