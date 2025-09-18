f9ed9e622ccf59cac21a22f1bfd7d425c3724c75
Lakshya <lakshyahindocha@gmail.com>
2025-09-18 23:27:42 +0530

    shopping cart

--- shopping_cart.js
+++ shopping_cart.js
@@ -1,4 +1,4 @@
-let book1 = prompt("Enter price of book1: ")
+let book1 = prompt("Enter price of book1: ");
 let book2 = prompt("Enter price of book2: ");
 let book3 = prompt("Enter price of book3: ");
 
@@ -13,23 +13,34 @@ let sub_tot3 = book3 * qty3;
 let total = sub_tot1 + sub_tot2 + sub_tot3;
 let shipping = 9.99;
 if (total >= 150) {
-    console.log("You are eligible for Free Shipping, Your total is: $"+total)
+  console.log("You are eligible for Free Shipping, Your total is: $" + total);
 } else {
-    console.log("Your total is: $"+total)
-    total+=shipping;
-    console.log("You have to pay shipping charges of 9.99, Your new total is: $"+total)
+  console.log("Your total is: $" + total);
+  total += shipping;
+  console.log(
+    "You have to pay shipping charges of 9.99, Your new total is: $" + total
+  );
 }
 let discount = 0;
 if (total >= 170 && total < 200) {
-    discount = total*0.05;
-    total -= discount;
-    console.log("You are eligible for a 5% discount\nYour total after discount is: $"+total)
+  discount = total * 0.05;
+  total -= discount;
+  console.log(
+    "You are eligible for a 5% discount\nYour total after discount is: $" +
+      total
+  );
 } else if (total >= 200 && total < 300) {
-    discount = total*0.1;
-    total -= discount;
-    console.log("You are eligible for a 10% discount\nYour total after discount is: $"+total)
+  discount = total * 0.1;
+  total -= discount;
+  console.log(
+    "You are eligible for a 10% discount\nYour total after discount is: $" +
+      total
+  );
 } else if (total >= 300) {
-    discount = total*0.15;
-    total -= discount;
-    console.log("You are eligible for a 15% discount\nYour total after discount is: $"+total)
+  discount = total * 0.15;
+  total -= discount;
+  console.log(
+    "You are eligible for a 15% discount\nYour total after discount is: $" +
+      total
+  );
 }
