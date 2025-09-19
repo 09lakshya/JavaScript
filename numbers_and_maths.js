d47f7e8abf63381baf3510a2254c704f8262e252
Lakshya <lakshyahindocha@gmail.com>
2025-09-19 20:20:49 +0530

    numbers and maths

---
+++ numbers_and_maths.js
@@ -0,0 +1,18 @@
+const num1 = 200000.123456;
+// console.log(num1);
+
+// console.log(num1.toString()); // converts number to string. You can perform all string operations here
+// console.log(num1.toFixed(3)); // the number given in the arguments is the no.of digits after decimal
+
+const num2 = 1000000;
+// console.log(num2.toLocaleString()); // returns the values with commas
+
+// ========================== MATHS =========================================
+
+console.log(Math);
+console.log(Math.abs(-4.5)); // returns +ve value (irrespective of sign given)
+console.log(Math.round(1.2)); // rounds of the number
+console.log(Math.ceil(2.1)); // rounds in the +ve direction
+console.log(Math.floor(8.9)); // rounds in the -ve direction
+
+console.log(Math.random()); // returns a random value between 0 and 1
